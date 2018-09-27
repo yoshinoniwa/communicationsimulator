b173f7b1e8144353b67ea84303c1c8393fb05f1e
yoshinoniwa <yoshinoniwa>
2018-09-27 14:19:07 +0900

    乱数生成

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.text.ParseException;
-
-public class Main {
-	public static void main(String args[]) throws ParseException{
-		System.out.println("Hello world");
-	}
-}
---
+++ src/RandomGenerator.java
@@ -0,0 +1,20 @@
+import java.text.ParseException;
+import java.util.Random;
+
+public class RandomGenerator {
+//	private double nextNextGaussian;
+//	private boolean haveNextNextGaussian = false;
+//	
+	//mainメソッド
+	public static void main(String args[]) throws ParseException{
+		Random r = new Random();
+		double time_ave; //タイミングの平均値
+		double time_sub; //タイミングの標準偏差
+		double data_ave; //データ量の秒平均
+		double data_sub; //データ量の標準偏差
+		System.out.println(r.nextGaussian());
+		
+	}
+	
+	
+}
